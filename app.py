f5a089f205ee7846dc4b12b0cbe18515a1ec6255
roppppo <92677122+roppppo@users.noreply.github.com>
2024-01-19 13:29:05 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+<html>
+    <h1>My second web based application : Image Generator by MidJourney</h1>
+    <form action="/" method="post">
+        <p>Please enter the image description : </p>
+        <p><input type="text" name="q"></p>
+        <p><input type="submit" value="Enter"></p>
+    </form>
+    <p>The link to image generated {{result}}</p>
+    <p><img src={{result}}></p>
+</html>
+============================================
+from flask import Flask,request,render_template
+import json,requests,time
+headers = {
+    "Authorization" : "Token r8_6173dF40oPfp1jwNAJ0Fc8qh9K1v6Ev1s5yBx",
+    "Content-Type" : "application/json"
+}
+app = Flask(__name__)
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("q")
+        print(q)
+        data = json.dumps(
+            {
+                "version" : "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                "input" : {"prompt":q}
+            }
+        )
+        r = requests.post("https://api.replicate.com/v1/predictions",data=data,headers=headers)
+        time.sleep(10)
+        r = r.json()["urls"]['get']
+        r = requests.post(r,headers=headers).json()["output"]
+        return(render_template("index.html",result=r[0]))
+    else:
+        return(render_template("index.html",result="waiting for rate to enter ........"))
+if __name__ == "__main__":
+    app.run()
